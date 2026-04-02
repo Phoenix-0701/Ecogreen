1f4b5832f7ef9219390c1869951e9c3d3974b47f
Phoenix-0701 <pntanh2k5@gmail.com>
2026-04-02 18:40:37 +0700

    Refactor AppController to handle telemetry messages
    Removed the eventsGateway dependency and updated message pattern handling.

--- ecogreen-server/src/app.controller.ts
+++ ecogreen-server/src/app.controller.ts
@@ -17,29 +17,11 @@
 
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { EventsGateway } from './events.gateway';
 
 @Controller()
 export class AppController {
-<<<<<<< HEAD
-  constructor(private readonly eventsGateway: EventsGateway) {}
-
-  @MessagePattern('ecogreen/test')
-  handleMqttMessage(@Payload() data: string) {
-    console.log(' CÓ THƯ TỪ ESP32 GỬI LÊN:', data);
-
-    // Gọi PrismaService để lưu data vào Database ở đây
-
-    //  Gửi tt lên cho FE
-    this.eventsGateway.broadcastSensorData({
-      source: 'ESP32',
-      message: data,
-      timestamp: new Date().toISOString(),
-    });
-=======
   @MessagePattern('ecogreen/telemetry')
   handleTelemetry(@Payload() data: any) {
     console.log('📬 Telemetry từ ESP32:', data);
->>>>>>> 43c3d7e50fb0208fb01d6b493a8361103a493718
   }
 }
