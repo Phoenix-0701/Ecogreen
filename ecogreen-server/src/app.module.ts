4972d4662b06e3a4eae1ba080fc392b93aca64d3
Phoenix-0701 <pntanh2k5@gmail.com>
2026-04-02 18:40:08 +0700

    Refactor AppModule to include AppController
    Removed EventsGateway and added AppController to the module.

--- ecogreen-server/src/app.module.ts
+++ ecogreen-server/src/app.module.ts
@@ -19,25 +19,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
-<<<<<<< HEAD
-import { EventsGateway } from './events.gateway';
-
-@Module({
-  imports: [
-    // DatabaseConfigModule,
-    UsersModule,
-    AuthModule,
-    PrismaModule,
-  ],
-  controllers: [],
-  providers: [EventsGateway],
-=======
 import { AppController } from './app.controller';
 
 @Module({
   imports: [UsersModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [],
->>>>>>> 43c3d7e50fb0208fb01d6b493a8361103a493718
 })
 export class AppModule {}
