7d39599ff02c594bd3e381d7be75210285f72ce8
Phoenix-0701 <pntanh2k5@gmail.com>
2026-04-02 18:53:57 +0700

    Update app.controller.ts

--- ecogreen-server/src/app.controller.ts
+++ ecogreen-server/src/app.controller.ts
@@ -17,11 +17,19 @@
 
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
+import { EventsGateway } from './events.gateway';
 
 @Controller()
 export class AppController {
+  constructor(private readonly eventsGateway: EventsGateway) {}
+
   @MessagePattern('ecogreen/telemetry')
   handleTelemetry(@Payload() data: any) {
     console.log('📬 Telemetry từ ESP32:', data);
+    this.eventsGateway.broadcastSensorData({
+      source: 'ESP32',
+      message: data,
+      timestamp: new Date().toISOString(),
+    });
   }
 }
