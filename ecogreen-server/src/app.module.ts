080aa3573c7e4d114952a704acf50ace678ebfa5
Phoenix-0701 <pntanh2k5@gmail.com>
2026-04-02 18:54:38 +0700

    Add EventsGateway to AppModule providers

--- ecogreen-server/src/app.module.ts
+++ ecogreen-server/src/app.module.ts
@@ -20,10 +20,10 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { AppController } from './app.controller';
-
+import { EventsGateway } from './events.gateway';
 @Module({
   imports: [UsersModule, AuthModule, PrismaModule],
   controllers: [AppController],
-  providers: [],
+  providers: [EventsGateway],
 })
 export class AppModule {}
